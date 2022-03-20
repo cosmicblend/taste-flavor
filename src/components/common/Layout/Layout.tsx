ed74ace9fea069beff451c72ddcf0088fd43e2f6
Chet Woodside <chet@cosmicblend.ca>
2022-03-19 18:30:35 -0700

    suppressing header

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -16,7 +16,6 @@ import useIsomorphicLayoutEffect from "hooks/useIsomorphicLayoutEffect"
 import Head from "next/head"
 import { PropsWithChildren, ReactNode, useRef, useState } from "react"
 import GuildLogo from "../GuildLogo"
-import Header from "./components/Header"
 
 type Props = {
   imageUrl?: string
